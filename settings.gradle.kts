4c1461e6c40e26db1062d31dcc8b159793de1274
hyein You <laksjd4560@gmail.com>
2024-01-14 17:11:13 +0900

    Start

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "positive_and_negative"
+include(":app")
+ 
